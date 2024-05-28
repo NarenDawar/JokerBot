003af38fd39103c1247aea337b2b63f5444db14a
NarenDaw <narendaw1@gmail.com>
2024-05-28 11:03:28 -0400

    Updated info slide.

--- DiscordBot/commands/bot-info.js
+++ DiscordBot/commands/bot-info.js
@@ -10,16 +10,16 @@ module.exports = {
         const {embedColor} = profileData;
         let informationBoard = new EmbedBuilder()
             .setTitle("Information About JokerBot")
-            .setDescription("JokerBot is a cohesive economy bot that holds a main focus on betting on various activities.\nJokerBot was created in May of 2024.\nBelow you can find the slash commands:")
+            .setDescription("JokerBot is a cohesive economy bot that holds a main focus on betting on various activities. Although there is certainly more to come.\nJokerBot was created in May of 2024.\nBelow you can find the slash commands with brief descriptions:")
             .setColor(embedColor)
             .addFields({
                 name: 'Games',
-                value: 'coinflip\nrock-paper-scissors\nblackjack\nroulette (colors and numbers)',
+                value: 'coinflip: Bet on head/tails.\nrock-paper-scissors: Bet on a game of rps against a computer.\nblackjack: Classic game of blackjack.\nroulette (colors and numbers): Can pick to bet on colors or numbers. Numbers has 5x initial bet payout if you win.\nhorse-race: Three horses are generated with 3 different probailities of winning. Select one to win. Probabilities change on every new occurance',
                 inline: true,
             },
             {
                 name:'Functionality',
-                value: 'balance\ndaily\ninfo\nleaderboard\ntransfer\nwork\nshop\nshop-info',
+                value: 'balance: Check your current balance\ndaily: Redeem daily reward\nuser-info: Check information of any user in the server (if registered).\nleaderboard: Check local leaderboard.\ntransfer: Transfer money to someone else.\nwork: Work for some money. Available once an hour.\nshop: Buy some cool aadd-ons. Shop in development.\nshop-info: Checks what is in the shop',
                 inline: true,
             },
             {
